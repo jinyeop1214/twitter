8e16bb18263256fcc629c5a3a40d7cf743f33310
jinyeop <bb3598@naver.com>
2022-08-19 01:08:57 +0900

    firebase auth

---
+++ src/components/Router.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+
+const AppRouter = ({ isLoggedIn }) => {
+	return (
+		<Router>
+			<Routes>
+				{isLoggedIn ? (
+					<>
+						<Route path="/" element={<Home />}></Route>
+					</>
+				) : (
+					<>
+						<Route path="/" element={<Auth />}></Route>
+					</>
+				)}
+			</Routes>
+		</Router>
+	);
+};
+
+export default AppRouter;
